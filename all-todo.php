bd3e6045001a2fc0e30e897b5dea5d02d350eba6
sharif <sorifulislambd2009@gmail.com>
2026-08-12 00:41:43 +0600

    Added new changes

--- all-todo.php
+++ all-todo.php
@@ -1,7 +1,7 @@
 <?php 
 include './include/header.php';
 include "database/env.php";
-$query ="SELECT * FROM `todos`";
+$query ="SELECT * FROM `todos` ORDER BY id DESC";
 $res = mysqli_query($conn,$query);
 $todos = mysqli_fetch_all($res, 1);
 
@@ -15,16 +15,16 @@ $todos = mysqli_fetch_all($res, 1);
      <div class="col-lg-4 mb-3">
       <div class="card">
       <div class="card-header"><?= $todo['title'] ?> - 
-      <span class="badge bg-success-subtle text-success"> <?= $todo['it_complete'] ? 'Completed' : 'Pending' ?> </span> 
+      <span class="badge bg-<?= $todo['it_complete'] ? 'success' : 'warning' ?>-subtle text-<?= $todo['it_complete'] ? 'success' : 'warning' ?>"> <?= $todo['it_complete'] ? 'Completed' : 'Pending' ?> </span> 
     </div>
       <div class="card-body">
         <p><?= $todo['description'] ?></p>
         <b> <?= $todo['deadline'] ?></b>
       </div>
       <div class="card-footer">
-       <a class="btn btn-sm btn-primary" href="">Complete</a>
-       <a class="btn btn-sm btn-warning" href="">Edit</a>
-       <a class="btn btn-sm btn-danger" href="">Delete</a>
+       <a class="<?= $todo['it_complete'] ? 'disabled' : ''  ?> btn btn-sm btn-primary" href="">Complete</a>
+       <a class="<?= $todo['it_complete'] ? 'disabled' : ''  ?> btn btn-sm btn-warning" href="">Edit</a>
+       <a class=" <?= $todo['it_complete'] ? 'disabled' : ''  ?> btn btn-sm btn-danger" href="">Delete</a>
       </div>
 
       </div>
